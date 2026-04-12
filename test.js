5436f1f24d4a747cdf8c062a141b31b34361cc1b
TongLee <tonglee0914@gmail.com>
2026-04-12 09:28:22 +0200

    First commit: Scrape Real Estate Data from Redfin. Data includes Pending, UnderContract and Sold-last1week.

--- test.js
+++
@@ -1,5 +0,0 @@
-const originalString = "Hello World! This is a test.";
-// const modifiedString = originalString.replace(/ /g, '-');
-const modifiedString = originalString.toLowerCase();
-
-console.log(modifiedString); // Output: "Hello-World!-This-is-a-test."
